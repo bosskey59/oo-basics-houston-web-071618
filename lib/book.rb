e06ef4cba9a2634e9bb740617bc7e1aa3af85323
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-18 23:04:07 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+  def initialize(title)
+    @title =title
+    @page_count =0
+    @genre=""
+  end
 
+  def title
+    @title
+  end
 
+  def author
+    @author
+  end
+
+  def author=(name)
+    @author=name
+  end
+
+  attr_accessor :page_count, :genre
+
+  def turn_page
+    puts"Flipping the page...wow, you read fast!"
+  end
+
+end
